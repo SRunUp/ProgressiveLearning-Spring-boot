8fbccb34379bbe7249e6924dcdb165e21980cb03
SRunUp <33618057+SRunUp@users.noreply.github.com>
2018-12-13 11:57:53 +0800

    翻译比较器

---
+++ translation-compare/src/main/java/com/translationcompare/TranslationCompareApplication.java
@@ -0,0 +1,14 @@
+package com.translationcompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationCompareApplication.class, args);
+    }
+
+}
+
